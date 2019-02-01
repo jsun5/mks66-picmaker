980dcf5044cc53d085e54e0462a3bbfa37c02714
jsun5 <jsun5@stuy.edu>
2019-02-01 00:14:30 -0500

    building final string to write into file

--- ppmGenerator.py
+++ ppmGenerator.py
@@ -1,10 +1,22 @@
 import random
 
-type = "P3"
-dimensions = "500 500"
-largest = "255"
+final = ""
+
+type = "P3\n"
+x = 500
+y = 500
+largest = "\n255\n"
+
+final+= type + str(x) + " " + str(y) + largest
 
 red = random.randint(255)
 green = random.randint(255)
 blue = random.randint(255)
 
+for i in range(x):
+	for g in range(y):
+		r = red
+		g = green
+		b = blue
+		final += str(r) + ' ' + str(g) + ' ' + str(b) + ' \t'
+		
