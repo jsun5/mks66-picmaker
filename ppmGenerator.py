d2fc4796523b0b46a13309b196e9cfb94b857793
jsun5 <jsun5@stuy.edu>
2019-02-01 00:59:18 -0500

    made weird pic, fixing up

--- ppmGenerator.py
+++ ppmGenerator.py
@@ -3,21 +3,21 @@ import random
 final = ""
 
 type = "P3\n"
-x = 500
-y = 500
+x = 200
+y = 200
 largest = "\n255\n"
 
 final+= type + str(x) + " " + str(y) + largest
 
-red = random.randint(255)
-green = random.randint(255)
-blue = random.randint(255)
+red = random.randint(0,255)
+green = random.randint(0,255)
+blue = random.randint(0,255)
 
 for i in range(x):
 	for g in range(y):
 		r = red
-		g = green
-		b = blue
+		g = green + (abs(green - i - g))
+		b = blue - abs(i-g)
 		final += str(r) + ' ' + str(g) + ' ' + str(b) + ' \t'
 	final += '\n'
 		
